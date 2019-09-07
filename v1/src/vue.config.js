eb90067c9a77b5029bd9fa08c8b05d3b643c772a
summuss <zongyang_liu@outlook.com>
2019-09-07 13:45:05 +0800

    add vue

---
+++ v1/src/vue.config.js
@@ -0,0 +1,22 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-09-06 23:31:27
+ * @LastEditTime: 2019-09-07 12:23:26
+ * @LastEditors: Please set LastEditors
+ */
+module.exports = {
+  // publicPath: 'v1',
+  // devServer: {
+  //   proxy: {
+  //     '/v1': {
+  //       target: 'http://localhost:8080/v1',
+  //       ws: true,
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         '^/v1': '/'
+  //       }
+  //     }
+  //   }
+  // }
+}
